b16216771f34dcf6787808f08780490ac0fa6110
waffleturtle <39573586+waffleturtle@users.noreply.github.com>
2019-04-22 17:10:46 -0500

    all functions work correctly except for TEST_NN

--- part2/neural_network.py
+++ part2/neural_network.py
@@ -26,17 +26,20 @@ def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_cl
     #IMPLEMENT HERE
     batch_size = 200
     losses = []         #a list of total loss at each epoch. Length = epoch
-    for e in range(1, epoch):
+    for e in range(epoch):  #or (1, epoch + 1). not sure
+        print("new epoch")
         if shuffle == True:
             np.random.shuffle(x_train)
         total_loss = 0
-        for i in range(1, int(len(x_train)/batch_size)): #number of examples / batch size
-            x = x_train     #not sure
-            y = y_train
-            loss, w1, w2, w3, w4 = four_nn(w1, w2, w3, w4, b1, b2, b3, b4, x, y, num_classes)
+        #add "+1" in range function to pass unit test
+        for i in range(1, int(len(x_train)/batch_size) + 1): #number of examples / batch size
+            x = x_train[(i - 1) * batch_size  :  i * batch_size]    # split x & y into smaller batches
+            y = y_train[(i - 1) * batch_size  :  i * batch_size]
+            #print("four_nn called for the " + str(i) + " times")
+            loss, w1, w2, w3, w4 = four_nn(w1, w2, w3, w4, b1, b2, b3, b4, x, y, num_classes, test = False)
             total_loss += loss
         losses.append(total_loss)
-        
+    print(losses)
     return w1, w2, w3, w4, b1, b2, b3, b4, losses
 
 """
@@ -55,11 +58,15 @@ def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_cl
         Good place to show your confusion matrix as well.
         The confusion matrix won't be autograded but necessary in report.
 """
-def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
 
+
+def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
+    classfication = four_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes, test = True)
+    print(classfication)
     avg_class_rate = 0.0
     class_rate_per_class = [0.0] * num_classes
     return avg_class_rate, class_rate_per_class
+
 
 """
     4 Layer Neural Network
@@ -67,7 +74,10 @@ def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
     Up to you on how to implement this, won't be unit tested
     Should call helper functions below
 """
-def four_nn(W1, W2, W3, W4, b1, b2, b3, b4, X, Y, num_classes): """X = x_train, Y = y_train"""
+
+
+def four_nn(W1, W2, W3, W4, b1, b2, b3, b4, X, Y, num_classes, test):
+    """X = x_train, Y = y_train"""
     Z1, acache1 = affine_forward(X,W1,b1)
     A1, rcache1 = relu_forward(Z1)
     Z2, acache2 = affine_forward(A1,W2,b2)
@@ -75,11 +85,15 @@ def four_nn(W1, W2, W3, W4, b1, b2, b3, b4, X, Y, num_classes): """X = x_train, 
     Z3, acache3 = affine_forward(A2,W3,b3)
     A3, rcache3 = relu_forward(Z3)
     F, acache4 = affine_forward(A3,W4,b4)
-    if (test == True):"""modify this condition"""
+
+    #print("four_nn halfway done")
+
+    if (test == True):
         classifications = np.zeros(F.shape[0])
         for i in range(F.shape[0]):
             classifications[i] = max(F[i])
         return classifications
+
     loss, dF = cross_entropy(F,Y)
     dA3, dW4, db4 = affine_backward(dF,acache4)
     dZ3 = relu_backward(dA3,rcache3)
@@ -93,73 +107,88 @@ def four_nn(W1, W2, W3, W4, b1, b2, b3, b4, X, Y, num_classes): """X = x_train, 
     W2 = W2 - eta*dW2
     W3 = W3 - eta*dW3
     W4 = W4 - eta*dW4
-    return loss,W1, W2, W3, W4  """may need modify this line"""
-    pass
+
+    return loss, W1, W2, W3, W4
 
 """
     Next five functions will be used in four_nn() as helper functions.
     All these functions will be autograded, and a unit test script is provided as unit_test.py.
     The cache object format is up to you, we will only autograde the computed matrices.
-
     Args and Return values are specified in the MP docs
     Hint: Utilize numpy as much as possible for max efficiency.
         This is a great time to review on your linear algebra as well.
 """
+
+
 def affine_forward(A, W, b):
-    
-    """Z=np.dot(A,W)+ b
+
     """
-    Z=np.zeros((A.shape[0],W.shape[1]))
+    Z = np.zeros((A.shape[0], W.shape[1]))
     for i in range(A.shape[0]):
         for j in range(W.shape[1]):
-            Z[i][j]+=b[j]
+            Z[i][j] += b[j]
             for k in range(A.shape[1]):
-                Z[i][j]+=A[i][k]*W[k][j]
-    cache = (A,W,b)
+                print("check if the program is stuck here")
+                Z[i][j] += A[i][k] * W[k][j]
+    """
+    Z = np.dot(A, W) + b        #dot product of 2 arrays
+    cache = (A, W, b)
     return Z, cache
 
+
 def affine_backward(dZ, cache):
-    A,W,b = cache[0], cache[1], cache[2]
-    """dA = np.dot(dZ,W.T)
+    A, W, b = cache[0], cache[1], cache[2]
+    dA = np.dot(dZ,W.T)
     dW = np.dot(A.T,dZ)
-    db = np.dot(np.ones(i),Z)"""
+    dB = sum(dZ)
+    """
     for i in range(A.shape[0]):
         for k in range(A.shape[1]):
             for j in range(Z.shape[1]):
-                dA[i][k] += dZ[i][j]*W[k][j]
-                dW[k][j] += A[i][k]*dZ[i][j]
+                dA[i][k] += dZ[i][j] * W[k][j]
+                dW[k][j] += A[i][k] * dZ[i][j]
                 db[j] += dZ[i][j]
+    """
     return dA, dW, dB
 
+
 def relu_forward(Z):
+    # was using for loops but too slow
+    A = np.maximum(Z, 0)    # Compare two arrays and returns a new array containing the element-wise maxima
     cache = Z
-    for i in range(Z.shape[0]):
-        for j in range(Z.shape[1]):
-            A[i][j] = max(elem,0)
     return A, cache
 
+
 def relu_backward(dA, cache):
+    # dZij = 0 if Zij = 0. else dZij = dAij.
+
     Z = cache
     dZ = dA
+    """
     for i in range(Z.shape[0]):
         for j in range(Z.shape[1]):
             dZ[i][j] = dZ
+    """
+    dZ[Z <= 0] = 0
     return dZ
 
+
 def cross_entropy(F, y):
-    loss = np.zeros(F.shape[0])
+    loss = 0
     dF = np.zeros(F.shape)
     l1 = np.zeros(F.shape[0])
     l2 = np.zeros(F.shape[0])
+    n = F.shape[0]      # size of F and n
     for i in range(F.shape[0]):
-        l1[i] += F[i][y[i]]
+        temp = int(y[i])
+        l1[i] += F[i][temp]     # use temp to avoid weird compile error
         for k in range(F.shape[1]):
-            l2[i] += exp(F[i][k])
+            l2[i] += np.exp(F[i][k])
         for j in range(F.shape[1]):
-            isLabel = j==y[i]
-            dF[i][j] = -1/n(isLabel-np.exp(F[i][j])/l2[i])
-        loss[i] = -1/n(l1[i]-np.log(l2[i]))
-    
-            
+            isLabel = j == y[i]
+            dF[i][j] = (-1 / n) * (isLabel - np.exp(F[i][j]) / l2[i])
+        loss += (-1 / n) * (l1[i] - np.log(l2[i]))
+
     return loss, dF
+
 
